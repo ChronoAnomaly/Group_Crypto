69f386fb8be3f15a3934ade3dd73b027539c0970
ChronoAnomaly <chronoanomaly@gmail.com>
2015-04-09 19:17:27 -0400

    Finished up getting the key to save to a file and be loaded from a file

--- src/Scothorn_RSA_TEST1/EncryptionTest.java
+++ src/Scothorn_RSA_TEST1/EncryptionTest.java
@@ -1,10 +1,8 @@
 package Scothorn_RSA_TEST1;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 /**
  * Created by brett on 2015-04-06.
@@ -13,29 +11,17 @@ public class EncryptionTest {
 
     public static void main(String[] args) {
 
-        EncryptionUtil sec = new EncryptionUtil();
+        EncryptionUtil sec = null;
+        try {
+            sec = new EncryptionUtil();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         try {
-
-
-//            final String originalText = "Text to be encrypted ";
-//            ObjectInputStream inputStream = null;
-//
-//            // Encrypt the string using the public key
-//            inputStream = new ObjectInputStream(new FileInputStream(sec.PUBLIC_KEY_FILE));
-//            final PublicKey publicKey = (PublicKey) inputStream.readObject();
-//            final byte[] cipherText = sec.encrypt(originalText, publicKey);
-//
-//            // Decrypt the cipher text using the private key.
-//            inputStream = new ObjectInputStream(new FileInputStream(sec.PRIVATE_KEY_FILE));
-//            final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
-//            final String plainText = sec.decrypt(cipherText, privateKey);
-//
-//            // Printing the Original, Encrypted and Decrypted Text
-//            System.out.println("Original: " + originalText);
-//            System.out.println("Encrypted: " +cipherText.toString());
-//            System.out.println("Decrypted: " + plainText);
 
             System.out.println("Testing file encrypting: ");
             File input = new File("To Encrypt.txt");
