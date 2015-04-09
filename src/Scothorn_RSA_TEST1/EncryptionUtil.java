892ee73c9ca5f4de1624503364c363095552ec67
ChronoAnomaly <chronoanomaly@gmail.com>
2015-04-09 19:17:27 -0400

    Finished up getting the key to save to a file and be loaded from a file

--- src/Scothorn_RSA_TEST1/EncryptionUtil.java
+++ src/Scothorn_RSA_TEST1/EncryptionUtil.java
@@ -27,17 +27,9 @@ public class EncryptionUtil {
     public final String ALGORITHM = "AES/ECB/PKCS5Padding";
 
     /**
-     * String to hold the name of the private key file.
+     * Location of the private key for the AES algorithm.
      */
-    public final String PRIVATE_KEY_FILE = "./keys/private.key";
-
-    /**
-     * String to hold name of the public key file.
-     */
-    public final String PUBLIC_KEY_FILE = "./keys/public.key";
-
-    public final String KEY_FILE =  "./keys/secure.keystore";
-    private String passTest = "TeStInG";
+    public final String KEY_FILE =  "./keys/secure.key";
 
     private final int BLOCK_SIZE = 1024;
     private final int AES_KEY_SIZE = 128;
@@ -48,37 +40,15 @@ public class EncryptionUtil {
     private SecretKeySpec aeskeySpec;
 
 
-    public EncryptionUtil() {
+    public EncryptionUtil() throws GeneralSecurityException, IOException {
 
-        try {
-
-
-
-
-            KeyStore keyStore = createKeyStore(KEY_FILE, passTest);
-
-            if (keyStore.size() == 0) {
-                generateKey();
-
-                // store the secret key
-                KeyStore.SecretKeyEntry keyStoreEntry = new KeyStore.SecretKeyEntry(aeskeySpec);
-                KeyStore.PasswordProtection keyPassword = new KeyStore.PasswordProtection("pw-secret".toCharArray());
-                keyStore.setEntry("aesSecretKey", keyStoreEntry, keyPassword);
-                keyStore.store(new FileOutputStream(KEY_FILE), passTest.toCharArray());
-            } else {
-
-                KeyStore.Entry entry = keyStore.getEntry("mySecretKey", null);
-                aeskeySpec = (SecretKeySpec) entry;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-//        if (!areKeysPresent()) {
-//            // Method generates a pair of keys using the RSA algorithm and stores it
-//            // in their respective files
+        if (!areKeysPresent()) {
+            // Method generates a pair of keys using the RSA algorithm and stores it
+            // in their respective files
             generateKey();
-//        }
+        } else {
+            loadKey();
+        }
     }
 
     /**
@@ -99,72 +69,57 @@ public class EncryptionUtil {
             aeskeySpec = new SecretKeySpec(aesKey, "AES");
             aesCipher = Cipher.getInstance(ALGORITHM);
 
+            saveKey();
+
+
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (NoSuchPaddingException e) {
             e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
         }
 
-//        try {
-//            final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-//            keyGen.initialize(1024);
-//            final KeyPair key = keyGen.generateKeyPair();
-//
-//            File privateKeyFile = new File(PRIVATE_KEY_FILE);
-//            File publicKeyFile = new File(PUBLIC_KEY_FILE);
-//
-//            // Create files to store public and private key
-//            if (privateKeyFile.getParentFile() != null) {
-//                privateKeyFile.getParentFile().mkdirs();
-//            }
-//            privateKeyFile.createNewFile();
-//
-//            if (publicKeyFile.getParentFile() != null) {
-//                publicKeyFile.getParentFile().mkdirs();
-//            }
-//            publicKeyFile.createNewFile();
-//
-//            // Saving the Public key in a file
-//            ObjectOutputStream publicKeyOS = new ObjectOutputStream(
-//                    new FileOutputStream(publicKeyFile));
-//            publicKeyOS.writeObject(key.getPublic());
-//            publicKeyOS.close();
-//
-//            // Saving the Private key in a file
-//            ObjectOutputStream privateKeyOS = new ObjectOutputStream(
-//                    new FileOutputStream(privateKeyFile));
-//            privateKeyOS.writeObject(key.getPrivate());
-//            privateKeyOS.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
     }
-    private static KeyStore createKeyStore(String fileName, String pw) throws Exception {
-        File file = new File(fileName);
 
-        final KeyStore keyStore = KeyStore.getInstance("JCEKS");
-        if (file.exists()) {
-            // .keystore file already exists => load it
-            keyStore.load(new FileInputStream(file), pw.toCharArray());
-        } else {
-            // .keystore file not created yet => create it
-            keyStore.load(null, null);
-//            keyStore.store(new FileOutputStream(fileName), pw.toCharArray());
-            // store away the keystore
-            java.io.FileOutputStream fos = null;
-            try {
-                fos = new java.io.FileOutputStream("newKeyStoreName");
-                keyStore.store(fos, pw.toCharArray());
-            } finally {
-                if (fos != null) {
-                    fos.close();
-                }
+    public void saveKey() throws IOException, GeneralSecurityException {
+
+
+            File privateKeyFile = new File(KEY_FILE);
+
+            // Create file to store private key
+            if (privateKeyFile.getParentFile() != null) {
+                privateKeyFile.getParentFile().mkdirs();
             }
-        }
+            privateKeyFile.createNewFile();
 
-        return keyStore;
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(privateKeyFile));
+        os.write(aesKey);
+        os.close();
+
     }
+
+    public void loadKey() throws GeneralSecurityException, IOException {
+
+
+        File privateKeyFile = new File(KEY_FILE);
+
+
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(privateKeyFile));
+
+        aesKey = new byte[AES_KEY_SIZE/8];
+        FileInputStream is = new FileInputStream(privateKeyFile);
+        BufferedInputStream bin = new BufferedInputStream(is);
+        in.read(aesKey);
+        aeskeySpec = new SecretKeySpec(aesKey, "AES");
+        aesCipher = Cipher.getInstance(ALGORITHM);
+
+    }
+
 
     /**
      * The method checks if the pair of public and private key has been generated.
@@ -180,68 +135,6 @@ public class EncryptionUtil {
         } else {
             return false;
         }
-    }
-
-//    private void saveKey() throws IOException {
-//
-//        ObjectOutputStream oout = new ObjectOutputStream(aeskeySpec);
-//        try {
-//            oout.writeObject(KEY_FILE);
-//        } finally {
-//            oout.close();
-//        }
-//
-//    }
-
-    /**
-     * Encrypt the plain text using public key.
-     *
-     * @param text
-     *          : original plain text
-     * @param key
-     *          :The public key
-     * @return Encrypted text
-     * @throws java.lang.Exception
-     */
-    public byte[] encrypt(String text, PublicKey key) {
-        byte[] cipherText = null;
-        try {
-            // get an RSA cipher object and print the provider
-            final Cipher cipher = Cipher.getInstance(ALGORITHM);
-            // encrypt the plain text using the public key
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            cipherText = cipher.doFinal(text.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return cipherText;
-    }
-
-    /**
-     * Decrypt text using private key.
-     *
-     * @param text
-     *          :encrypted text
-     * @param key
-     *          :The private key
-     * @return plain text
-     * @throws java.lang.Exception
-     */
-    public String decrypt(byte[] text, PrivateKey key) {
-        byte[] decryptedText = null;
-        try {
-            // get an RSA cipher object and print the provider
-            final Cipher cipher = Cipher.getInstance(ALGORITHM);
-
-            // decrypt the text using the private key
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            decryptedText = cipher.doFinal(text);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return new String(decryptedText);
     }
 
     /*
